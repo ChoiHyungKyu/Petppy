b72e7cc9b50fa7de1aeb225a8b979e079857b986
Hyungkyu Choi <fab@kakao.com>
2020-07-25 22:37:12 +0900

    Initial Splash Activity

---
+++ app/src/main/java/com/fab/sleepwake/Splash.java
@@ -0,0 +1,33 @@
+package com.fab.sleepwake;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        ImageView splash = (ImageView)findViewById(R.id.splashImage);
+        Glide.with(this).load(R.drawable.splash_image).into(splash);
+        Handler hd = new Handler();
+        hd.postDelayed(new splashHander(), 2000);
+    }
+
+    private class splashHander implements Runnable{
+        public void run(){
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            Splash.this.finish();
+        }
+    }
+    @Override
+    public void onBackPressed(){
+        // Nothing to do
+    }
+}
