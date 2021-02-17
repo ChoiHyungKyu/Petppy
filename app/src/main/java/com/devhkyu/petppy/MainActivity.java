4363c6cbd2c585275e706ca63bbd64bc3ec19544
Hyungkyu Choi <fab@kakao.com>
2021-02-17 20:08:07 +0900

    Album Test + HardwareAccelerated=False
    Warning: Manifest fixed (HWAcc=False)

--- app/src/main/java/com/devhkyu/petppy/MainActivity.java
+++ app/src/main/java/com/devhkyu/petppy/MainActivity.java
@@ -1,21 +1,55 @@
 package com.devhkyu.petppy;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkSelfPermission();
+        btn = findViewById(R.id.btn);
+        iv = findViewById(R.id.iv);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 101);
+            }
+        });
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -25,6 +59,49 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        if(requestCode == 1){
+            int length = permissions.length;
+            for(int i = 0; i<length; i++){
+                if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                    Log.d("MainActivity", "권한 허용: " +permissions[i]);
+                }
+            }
+        }
+    }
+
+    public void checkSelfPermission(){
+        String temp = "";
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+        }
+        if(TextUtils.isEmpty(temp) == false){
+            ActivityCompat.requestPermissions(this, temp.trim().split(" "), 1);
+        }else{
+            Toast.makeText(this, "권한을 모두 혀용", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if(requestCode == 101 && resultCode == RESULT_OK){
+            try{
+                InputStream is = getContentResolver().openInputStream(data.getData());
+                Bitmap bm = BitmapFactory.decodeStream(is);
+                is.close();
+                iv.setImageBitmap(bm);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }else if(requestCode == 101 && resultCode == RESULT_CANCELED){
+            Toast.makeText(this, "취소", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
